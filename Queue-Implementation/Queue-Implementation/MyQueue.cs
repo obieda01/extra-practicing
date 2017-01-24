614a4eba779fd19a67ff4a69be6595a7de730a18
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-01-24 17:25:35 -0500

    my Queue implementation

---
+++ Queue-Implementation/Queue-Implementation/MyQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Implementation
+{
+    public class MyQueue<T>
+    {
+         class QueueNode<T>
+        {
+            public T value;
+            public QueueNode<T> next;
+            public QueueNode(T value) { this.value = value; }
+        }
+        QueueNode<T> last;
+        QueueNode<T> first;
+
+
+        public MyQueue() { }
+        //public MyQueue(T value) { this.value = value; }
+        public void enqueue(T value)
+        {
+            QueueNode<T> temp = new QueueNode<T>(value);
+            if (last!=null)
+            {
+                last.next = temp;
+            }
+            last = temp;
+            
+        }
+
+        public void printAll()
+        {
+            MyQueue<T> temp = head;
+            while (temp!=null)
+            {
+                Console.WriteLine(temp.value);
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ Queue-Implementation/Queue-Implementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyQueue<string> queue = new MyQueue<string>();
+
+            queue.enqueue("Ahmad1");
+            queue.enqueue("Ahmad2");
+            queue.enqueue("Ahmad3");
+            queue.printAll();
+
+        }
+    }
+}
