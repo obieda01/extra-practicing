efb6a5bb91a0d6e6b98455f62f3cb76accd49c50
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-01-25 14:17:21 -0500

    Stack Implementation

---
+++ Stack-Implementation/Stack-Implementation/MyStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    public class MyStack<T>
+    {
+        public class MyStackNode<T>
+        {
+             public T value;
+            public MyStackNode<T> next;
+            public MyStackNode(T value) {this.value=value;}
+        }
+
+        MyStackNode<T> head;
+
+        public void push(T value)
+        {
+            MyStackNode<T> temp = new MyStackNode<T>(value);
+            if (head != null)
+            {
+                temp.next = head;
+            }
+            head = temp;
+        }
+
+        public T pop()
+        {
+            if (head != null)
+            {
+                T val = head.value;
+                head = head.next;
+                return val;
+            }
+            else throw new FieldAccessException();
+        }
+
+
+        public void printAll()
+        {
+            MyStackNode<T> temp = head;
+            while (temp!=null)
+            {
+                Console.WriteLine(temp.value);
+                temp = temp.next;
+
+            }
+        }
+    }
+}
+   
+
---
+++ Stack-Implementation/Stack-Implementation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyStack<string> st = new MyStack<string>();
+
+            st.push("Ahmad1");
+            st.push("Ahmad2");
+            st.push("Ahmad3");
+
+            st.printAll();
+
+        }
+    }
+}
