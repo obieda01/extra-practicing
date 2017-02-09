0fba439df7abee297b8e618592bdb22723e18617
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-02-08 23:15:15 -0500

    FizzBuzz text creations

---
+++ CrackingInterview/CrackingInterview/CrackingInterview.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingInterview.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        CrackingInterview s=new CrackingInterview()
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
