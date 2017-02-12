e33e5e46db72ba132410d7f46328de8fe08484de
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-02-12 02:14:56 -0500

    more Exercises solutions

--- CrackingInterview/CodeInterviewSolutions/AllMethodsTests/Tests.cs
+++ CrackingInterview/CodeInterviewSolutions/AllMethodsTests/Tests.cs
@@ -19,5 +19,35 @@ namespace AllMethodsTests
         {
             CollectionAssert.AreEqual(new int[] { 4,3,2,1 }, actualInstanceToTest.reverseArray(new int[] { 1,2,3,4 }));
         }
+        [TestMethod()]
+        public void isUnique_Test()
+        {
+            Assert.AreEqual(true, actualInstanceToTest.allUnique("Ahmad"));
+            Assert.AreEqual(false, actualInstanceToTest.allUnique(""));
+            Assert.AreEqual(false, actualInstanceToTest.allUnique("aa"));
+            Assert.AreEqual(true, actualInstanceToTest.allUnique("alA"));
+
+        }
+
+        [TestMethod()]
+        public void isPerm_Test()
+        {
+            Assert.AreEqual(true, actualInstanceToTest.isPermutation("Ahmad","hmadA"));
+            Assert.AreEqual(false, actualInstanceToTest.isPermutation("","p"));
+            Assert.AreEqual(false, actualInstanceToTest.isPermutation("P","p"));
+            Assert.AreEqual(true, actualInstanceToTest.isPermutation("cat", "atc"));
+        }
+        [TestMethod()]
+        public void editStringTest()
+        {
+          // Assert.AreEqual(new char[] {'a','%','2','0','h','%', '2', '0','m' }, actualInstanceToTest.editString("a h m    ",5));
+        }
+
+        [TestMethod]
+        public void compressString_Tests()
+        {
+            Assert.AreEqual("ahmad", actualInstanceToTest.compressString("ahmad"));
+            Assert.AreEqual("a4",actualInstanceToTest.compressString("aaaa"));
+        }
     }
 }
--- CrackingInterview/CodeInterviewSolutions/CodeInterviewSolutions/AllMethods.cs
+++ CrackingInterview/CodeInterviewSolutions/CodeInterviewSolutions/AllMethods.cs
@@ -21,14 +21,111 @@ namespace CodeInterviewSolutions
         }
         public int[] reverseArray(int[] sourceArray)
         {
-            for (int i = 0; i < sourceArray.Length/2; i++)
+            for (int i = 0; i < sourceArray.Length / 2; i++)
             {
                 int chacheValue = sourceArray[i];
-                sourceArray[i] = sourceArray[sourceArray.Length - 1-i];
-                sourceArray[sourceArray.Length - 1-i] = chacheValue;
+                sourceArray[i] = sourceArray[sourceArray.Length - 1 - i];
+                sourceArray[sourceArray.Length - 1 - i] = chacheValue;
             }
 
             return sourceArray;
+        }
+        public bool allUnique(string sourceString)
+        {
+
+            if (sourceString == null || sourceString.Length > 28 || sourceString.Length == 0) return false;
+            HashSet<char> charDictionary = new HashSet<char>();
+            foreach (char c in sourceString.ToCharArray())
+            {
+                if (charDictionary.Contains(c)) return false;
+                charDictionary.Add(c);
+            }
+            return true;
+        }
+
+        public bool isPermutation(string str1, string str2)
+        {
+            if (str1.Length != str2.Length || str2 == null || str1 == null) return false;
+            int[] charCount = new int[255];
+            foreach (char c in str1.ToCharArray())
+            {
+                charCount[c]++;
+            }
+            foreach (char c in str2.ToCharArray())
+            {
+
+                if (--charCount[c] < 0) return false;
+            }
+            return true;
+        }
+
+        public char[] reverseStirng(string sourceSting)
+        {
+
+            if (sourceSting == null) return null;
+            if (sourceSting.Length == 0) return new char[]{ };
+            char[] stringArray = sourceSting.ToCharArray();
+            for (int i = 0; i < stringArray.Length / 2; i++)
+            {
+                char lastChar = stringArray[stringArray.Length - 1 - i];
+                stringArray[stringArray.Length-1-i] = stringArray[i];
+                stringArray[i] = lastChar;
+            }
+            
+            return stringArray;
+        }
+
+        public char[] editString(string sourceString, int length)
+        {
+            int spaceCount = 0;
+            char[] stringChar = sourceString.ToCharArray();
+            for (int i = 0; i < length; i++)
+            {
+                if (stringChar[i] == ' ') spaceCount++; 
+            }
+            int newLength = length + spaceCount * 2;
+            for (int i = length-1; i >0; i--)
+            {
+                if (stringChar[i] != ' ')
+                {
+                    stringChar[newLength - 1] = stringChar[i];
+                    newLength--;
+                }
+                else
+                {
+                    stringChar[newLength - 1] = '0';
+                    stringChar[newLength - 2] = '2';
+                    stringChar[newLength - 3] = '%';
+                    newLength -= 3;
+                }
+            }
+
+            return stringChar;
+
+        }
+
+        public string compressString(string sourceString)
+        {
+            StringBuilder sb =  new StringBuilder();
+            char[] charString = sourceString.ToCharArray();
+            int counter = 1;
+
+            for (int i = 0; i < sourceString.Length-1; i++)
+            {
+                if (charString[i] == charString[i + 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    sb.Append(charString[i].ToString());
+                    sb.Append(counter.ToString());
+                    counter = 1;
+                }
+            }
+            sb.Append(charString[i].ToString());
+            sb.Append(counter.ToString());
+            return sb.Length >= charString.Length ? sourceString : sb.ToString();
         }
     }
 }
